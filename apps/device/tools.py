ce05ee398eb8c31395688a0abdc1af976adf307f
joe <wennjoe@163.com>
2022-04-18 09:08:25 +0800

    test

---
+++ apps/device/tools.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2022/4/18 9:07 AM
+# @Author   : Joe
+# @Site     :
+# @File     : tools.py.py
+# @Software : PyCharm
+# @function : xxxx
+
+from django.test.client import Client
